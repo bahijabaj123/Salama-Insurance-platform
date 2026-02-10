b6c8c4802779729ff3eb39e6ff6e44f49d5f6621
Bahija123 <benabdeljalilbahija@gmail.com>
2026-02-10 15:01:53 +0100

    first commit

---
+++ src/main/java/org/example/salamainsurance/SalamaInsuranceApplication.java
@@ -0,0 +1,13 @@
+package org.example.salamainsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalamaInsuranceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SalamaInsuranceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/salamainsurance/SalamaInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.salamainsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalamaInsuranceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
