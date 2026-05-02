94dde2419c6e61fe6dbee9bd3acee54d5c830307
bahijabaj123 <benabdeljalilbahija@gmail.com>
2026-05-02 17:52:52 +0100

    cleanup

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  /** Local backend (ng serve development build). */
-  apiBaseUrl: 'http://localhost:8082'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  /** API origin without trailing slash. Update for production deployments. */
-  apiBaseUrl: 'http://localhost:8082'
-};
